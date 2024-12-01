e5bb31c8a00d7511756f405823a65ce5c5f97d28
Zaimz <hafizahmadzaimkuntoro@gmail.com>
2024-12-01 23:11:57 +0700

    Percobaan 4 dan 5

---
+++ PengunjungCafe06.java
@@ -0,0 +1,11 @@
+public class PengunjungCafe06 {
+    public static void daftarPengunjung (String... namaPengunjung) {
+        System.out.println("Daftar nama pengunjung:");
+        for (String nama : namaPengunjung) {
+            System.out.println("- " + nama);
+        }
+    }
+    public static void main(String[] args) {
+        daftarPengunjung("Ali", "Budi", "Citra");
+    }
+}
